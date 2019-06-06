07c683953af1a5b48dc9053b141fd30b8b50e2cd
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-06 15:43:03 -0500

    More building

---
+++ Interfaces/Carnivores.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace diggers_and_fliers {
+
+    public interface IEatMeat
+    {
+        string EattheMeat { get; set; }
+
+        void MeatTreat();
+    }
+
+}
---
+++ Interfaces/PlantFolks.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace diggers_and_fliers {
+
+    public interface IEatPlants
+    {
+        string YayPlants { get; set; }
+
+        void Salad();
+    }
+
+}
